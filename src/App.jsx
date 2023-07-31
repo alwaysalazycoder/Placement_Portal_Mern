de30777389dea74ecf924239c6e1bdd9d94701fc
alwaysalazycoder <gs314315316@gmail.com>
2023-07-31 09:37:47 +0530

    "minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="bg-red-200 w-full h-[200px] font-bold flex items-center justify-center">
       <div className="bg-lime-300 text-black p-5 cursor-pointer">
-        Hello everyone
+        Hello everyone !!
       </div>
     </div>
   );
