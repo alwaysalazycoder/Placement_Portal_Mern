014fe5a8dcb1e61f95f584578db29d4f4dbd9de1
alwaysalazycoder <gs314315316@gmail.com>
2023-07-31 09:30:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-200 w-full h-[200px] font-bold flex items-center justify-center">
+      <div className="bg-lime-300 text-black p-5 cursor-pointer">
+        Hello everyone
+      </div>
+    </div>
+  );
+}
+
+export default App;
